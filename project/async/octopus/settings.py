4247954d1f46eaab9f18f9e7b3fe04ec720d7b3b
Dmytro Kovalchuk <dima.kovalchuk.v@gmail.com>
2024-07-13 15:42:34 +0300

    del unused

--- project/async/octopus/settings.py
+++ project/async/octopus/settings.py
@@ -138,5 +138,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-GRAPHENE = {"SCHEMA": "api.schema.schema"}
